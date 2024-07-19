16cae1c607f3bddd07676970809404a64880970d
Gilbert Jeiziner <gilbert.jeiziner@gjn.ch>
2024-07-19 08:56:11 +0200

    Adding guake script to open desired tabs

---
+++ guake-tabs.sh
@@ -0,0 +1,6 @@
+guake -n ~/code/config-swipos -r "config"
+guake -n ~/code/infra-packer-swipos-dev -r "packer-dev"
+guake -n ~/code/infra-packer-swipos -r "packer-PROD"
+guake -n ~/code/infra-terraform-swipos-dev -r "terraform-dev"
+guake -n ~/code/infra-terraform-swipos -r "terraform-PROD"
+guake -s 0
